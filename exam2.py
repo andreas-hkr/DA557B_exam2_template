ecae34073c3d4b6648d686112ad5debc7d17a3c2
Andreas Nilsson <andreas.nilsson@hkr.se>
2024-05-13 12:04:22 +0200

    Initial commit

---
+++ exam2.py
@@ -0,0 +1,27 @@
+def read_file(filename):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def write_file(filename, data):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def filter_numbers(data, keep_even):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def print_results(percentages):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def main():
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+if __name__ == '__main__':
+    main()
